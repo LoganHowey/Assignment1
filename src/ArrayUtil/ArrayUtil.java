18d1cd54a29d4d7906a43f861da10db3bb751f9a
LoganHowey <89224766+LoganHowey@users.noreply.github.com>
2022-08-19 16:05:38 -0600

    Initial commit

---
+++ src/ArrayUtil/ArrayUtil.java
@@ -0,0 +1,26 @@
+package ArrayUtil;
+
+public class ArrayUtil {
+
+    public void fill(int[] toFill, int val ){
+        toFill = new int[]{val};
+        val = 1,2,3,4,5;
+
+
+
+
+    }
+
+    public int[] copyOfRange(int[] original, int from, int to){
+        return new int[]{1};
+    }
+
+    public int sum(int[] addends){
+        return 0;
+    }
+
+    public int sum(int[][] addends){
+        return 0;
+    }
+
+}
