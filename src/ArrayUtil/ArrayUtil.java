87c241352671dde29d3fbe0aa34d4c7e6c389fe6
LoganHowey <89224766+LoganHowey@users.noreply.github.com>
2022-08-19 16:26:28 -0600

    started

--- src/ArrayUtil/ArrayUtil.java
+++ src/ArrayUtil/ArrayUtil.java
@@ -1,16 +1,13 @@
 package ArrayUtil;
 
+import java.util.Arrays;
+
 public class ArrayUtil {
 
-    public void fill(int[] toFill, int val ){
-        toFill = new int[]{val};
-        val = 1,2,3,4,5;
-
-
-
-
+    public static int[] fill(int[] toFill, int val){
+        toFill = new int[10];
+        for
     }
-
     public int[] copyOfRange(int[] original, int from, int to){
         return new int[]{1};
     }
@@ -23,4 +20,8 @@ public class ArrayUtil {
         return 0;
     }
 
+    public static void main(String[] args) {
+       int[] fill1 =  fill( 1);
+        System.out.println(Arrays.toString(fill1));
+    }
 }
